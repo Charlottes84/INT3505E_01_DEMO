bf2516192b96e946d4d407a7ac94b4863f50f5ce
Charlottes84 <hungnguyenduc023@gmail.com>
2025-10-22 15:02:08 +0700

    added demo webhook

--- DEMO_T03/Webhook/README.md
+++ DEMO_T03/Webhook/README.md
@@ -1,4 +1,12 @@
 # Webhook (Extensibility)
-- Extensibility đề cập đến khả năng cho phép bên ngoài (third parites) tùy chỉnh hoặc thêm chức năng mới vào API thông qua cơ chế được thiết kế sẵn (như Webhook, Plugin hoặc Custom Field)
+- **Extensibility** đề cập đến khả năng cho phép bên thứ 3 (third parites) tùy chỉnh hoặc thêm chức năng mới vào API thông qua cơ chế được thiết kế sẵn (như Webhook, Plugin hoặc Custom Field)
 - Nó cho phép API thay đổi hoặc phát triển mà không cần sửa đổi mã nguồn cốt lõi để đáp ứng một nhu cầu tùy chỉnh cụ thể 
-    - Ví dụ về Webhook Extensibility : 
+- **Webhook** là một cơ chế cho phép một ứng dụng tự động gửi thông tin (dữ liệu) đến một ứng dụng ngay lập tức khi có một sự kiện cụ thể xảy ra, mà không cần bên nhận phải phải liên tục hỏi thăm (polling)
+    - Ví dụ : Người dùng đăng nhập vào App A để xem giá Iphone có mức giá 25 triệu, người dùng muốn khi nào giá giảm xuống còn 20 triệu thì gửi thông báo cho người dùng mà không cần đăng nhập vào để xem giá, đó là việc sử dụng Webhook 
+## Demo Webhook 
+- Demo về quản lý sách : Người dùng sẽ mượn sách ở App A và khi đến ngày trả sách thì người dùng sẽ nhận được thông báo đến ngày trả sách mà không cần đăng nhập vào App A để xem đã hết hạn hay chưa
+```python
+api.add_resource(BorrowBookAPI, '/api/borrow')
+api.add_resource(CheckDueDateAPI, '/api/check_due_dates')
+api.add_resource(NotificationWebhook, '/webhook/notify') 
+```
--- DEMO_T03/Webhook/webhook.py
+++ DEMO_T03/Webhook/webhook.py
@@ -1,55 +1,95 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
+import requests
 import datetime
 
 app = Flask(__name__)
 api = Api(app)
 
 event_log = [] # demo = thay cho database
+borrowed_books_db = []
 
 # App A = Postman
 # App B = My laptop 
 # webhook.py = xu ly webhook
 # terminal = thong bao se nhan duoc 
 
-class BookEventWebhook(Resource):
+class BorrowBookAPI(Resource):
+    
     def post(self):
         data = request.get_json()
+        user_id = data.get('user_id')
+        book_id = data.get('book_id')
 
-        if not data or 'event_type' not in data or 'payload' not in data:
-            return {'message': 'Bad Request: Missing event_type or payload'}, 400
-        
-        event_type = data.get('event_type')
-        payload = data.get('payload')
+        due_date = (datetime.date.today() + datetime.timedelta(days=7)).isoformat()
 
-        log_entry = {
-            "received_at": datetime.datetime.now().isoformat(),
-            "event": event_type,
-            "data": payload
+        borrow_record = {
+            "user_id": user_id,
+            "book_id": book_id,
+            "due_date": due_date,
+            "notified": False
         }
-        event_log.append(log_entry)
 
-        # Check webhook received --> in terminal 
-        print("--- WEBHOOK RECEIVED ---")
-        print(f"Event: {event_type}")
-        print(f"User ID: {payload.get('user_id')}")
-        print(f"Book ID: {payload.get('book_id')}")
-        print("--------------------------------")
+        borrowed_books_db.append(borrow_record)
 
-        # logic 
-        if event_type == 'book_borrowed': 
-            # them logic : cap nhat CSDL 
-            # thong bao la da muon duoc sach
-            print(f"Processing BORROW event for book {payload.get('book_id')}")
-        elif event_type == 'book_returned':
-            print(f"Processing RETURN event for book {payload.get('book_id')}")
-        elif event_type == 'book_lost':
-            print(f"Processing LOST event for book {payload.get('book_id')}")
-        else:
-            print(f"Warning: Received unknown event_type '{event_type}'")
-        return {'status': 'success', 'message': f'Event {event_type} received'}, 200
+        print(f"\n--- 1. API /api/borrow ĐÃ LƯU ---")
+        print(f"User '{user_id}' đã mượn sách '{book_id}'. Hạn trả: {due_date}")
 
-api.add_resource(BookEventWebhook, '/webhook')
+        return {
+            "message": "Mượn sách thành công!",
+            "record": borrow_record
+        }, 200 
+    
+class CheckDueDateAPI(Resource):
+    def post(self): 
+        print(f"\n--- 2. HỆ THỐNG BẮT ĐẦU QUÉT HẠN TRẢ SÁCH ---")
+        
+        books_to_notify = [
+            book for book in borrowed_books_db if not book.get("notified")
+        ]
 
-if __name__ == '__main__':
+        if not books_to_notify:
+            print("Không có sách nào cần thông báo")
+            return {"message": "Không có sách nào cần thông báo"}, 200 
+        
+        for book in books_to_notify:
+            print(f"Phát hiện sách '{book['book_id']}' sắp hết hạn. Đang gọi Webhook thông báo...")
+
+            try:
+                requests.post(
+                    "http://127.0.0.1:5000/webhook/notify",
+                    json={
+                        "event_type":"due_date_reminder",
+                        "payload": {
+                            "user_id": book['user_id'],
+                            "book_id": book['book_id'],
+                            "due_date": book["due_date"],
+                            "message": f"Sách {book['book_id']} của bạn sắp hết hạn vào {book['due_date']}!"                        
+                        }
+                    }, 
+                    timeout=1
+                )
+                book["notified"]=True
+            except requests.exceptions.RequestException as e:
+                print(f"Lỗi khi gọi Webhook: {e}")
+        return {"message": f"Đã quét và gửi {len(books_to_notify)} thông báo."}, 200
+    
+class NotificationWebhook(Resource):
+    def post(self):
+        data = request.get_json()
+        payload = data.get('payload', {})
+
+        print(f"\n---  3. WEBHOOK NOTIFY ĐÃ NHẬN LỆNH  ---")
+        print(f"Event: {data.get('event_type')}")
+        print(f"Đang gửi thông báo cho: {payload.get('user_id')}")
+        print(f"Nội dung: {payload.get('message')}")
+        print("----------------------------------------------------")
+        
+        return {'status': 'success'}, 200
+    
+api.add_resource(BorrowBookAPI, '/api/borrow')
+api.add_resource(CheckDueDateAPI, '/api/check_due_dates')
+api.add_resource(NotificationWebhook, '/webhook/notify')
+
+if __name__ == "__main__":
     app.run(debug=True)
