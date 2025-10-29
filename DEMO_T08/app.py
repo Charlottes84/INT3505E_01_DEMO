c0c02794e09d1841f2711a13a585225c0a63306f
Charlottes84 <hungnguyenduc023@gmail.com>
2025-10-29 11:32:03 +0700

    (pc) added eager loading and batch loading for n + 1 queries

--- DEMO_T08/app.py
+++ DEMO_T08/app.py
@@ -4,15 +4,16 @@ from flask_restful import Resource, Api, marshal_with, fields
 from sqlalchemy.orm import joinedload, selectinload 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://book_management.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///book_management.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 api = Api(app)
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
-    books = db.relationship('Book', backref='authoor', lazy='select')
+    books = db.relationship('Book', backref='author', lazy='select')
 
     def __repr__(self):
         return f'<Author {self.name}>'
@@ -20,7 +21,7 @@ class Author(db.Model):
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120), nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('author_id'), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
 
     def __repr__(self):
         return f'<Book {self.title}>'
@@ -58,6 +59,39 @@ book_fields = {
     'author': fields.Nested(author_fields), 
 }
 
+# N + 1 queries 
+class BookList_NPlusOne(Resource):
+    @marshal_with(book_fields)
+    def get(self):
+        books = Book.query.all()
+
+        return books 
+
+api.add_resource(BookList_NPlusOne, '/books/n+1')
+
+
+# Joined Loading
+class BookList_Joined(Resource):
+    @marshal_with(book_fields)
+    def get(self):
+        books = db.session.excute(
+            db.select(Book).options(joinedload(Book.author))
+        ).scalars().all()
+
+        return books
+    
+api.add_resource(BookList_Joined, '/books/joined') 
+
+# Batch loading 
+class BookList_SelectIn(Resource):
+    @marshal_with(book_fields)
+    def get(self):
+        books = db.session.excute(
+            db.select(Book).options(selectinload(Book.author))
+        ).scalars.all()
+
+api.add_resource(BookList_SelectIn, '/books/selectin')
+
 if __name__ == '__main__':
     init_db()
-    
+    app.run(debug=True)
